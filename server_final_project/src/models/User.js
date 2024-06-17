77ed87fcf36b00f803799ba7fa01ceb2afe65e92
SheviEngelsberg <s4161823@gmail.com>
2024-06-17 17:50:28 +0300

    Updating the user model and adding validations

--- server_final_project/src/models/User.js
+++ server_final_project/src/models/User.js
@@ -1,10 +1,31 @@
 const mongoose = require('mongoose');
-const UserType = require('./UserType');
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    password: { type: String, required: true },
-    type: { type: String, enum: Object.values(UserType), required: true }
+    name: { 
+        type: String, 
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /^[a-zA-Z]+ [a-zA-Z]+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid full name! Must include first and last name separated by space.`
+        }
+    },
+    password: { 
+        type: String, 
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid password! Must include at least one digit, one lowercase, one uppercase letter, and one special character.`
+        }
+    },
+    type: { 
+        type: String, 
+        enum: [ 'user','shopManager', 'siteAdmin'], 
+        required: true 
+    }
 });
 
 const User = mongoose.model('User', userSchema);
