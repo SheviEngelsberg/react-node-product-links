c9ac7e1d4fc6bb1b3fa9ac18d7bc49db6af5eaf9
SheviEngelsberg <s4161823@gmail.com>
2024-06-20 00:55:08 +0300

    Adding middleware of tests on user as well as correcting the required actions

--- server_final_project/src/routes/userRoutes.js
+++ server_final_project/src/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const { verifyToken, isSiteAdmin } = require('../middlewares/authMiddleware');
+const { validUser, existUser, notExistUser } = require('../middlewares/validtionUserMiddelware')
 
 // Route to get all users (accessible only to SITEADMIN)
 router.get('/getAllUsers', verifyToken, isSiteAdmin, userController.getAllUsers);
@@ -10,13 +11,13 @@ router.get('/getAllUsers', verifyToken, isSiteAdmin, userController.getAllUsers)
 router.get('/getUser/:userId', verifyToken, isSiteAdmin, userController.getUserById);
 
 // Route to create a new user (accessible to all)
-router.post('/signUp', userController.signUp);
+router.post('/signUp', validUser, notExistUser, userController.signUp);
 
 // Route to sign in (accessible to all)
-router.post('/signIn', userController.signIn);
+router.post('/signIn', validUser, existUser, userController.signIn);
 
 // Route to update a user (accessible to all logged-in users)
-router.put('/updateUser/:userId', verifyToken, userController.updateUserById);
+router.put('/updateUser/:userId', verifyToken, validUser, notExistUser, userController.updateUserById);
 
 // Route to delete a user (accessible to all logged-in users)
 router.delete('/deleteUser/:userId', verifyToken, userController.deleteUserById);
