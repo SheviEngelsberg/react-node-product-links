bbd3e3f392139810dfcf28746f30eceabd498e9e
SheviEngelsberg <s4161823@gmail.com>
2024-06-25 02:30:22 +0300

    Changing the function isMyShopManager so that it is also true for the site manager who is above a shop manager

--- server_final_project/src/middlewares/authMiddleware.js
+++ server_final_project/src/middlewares/authMiddleware.js
@@ -32,19 +32,41 @@ const isSiteAdmin = async (req, res, next) => {
 };
 
 const isMyShopManager = async (req, res, next) => {
-    try {
-      const user = await User.findById(req.user.userId);
-      const shop = await Shop.findById(req.params.shopId);
-      if (!user || user.type !== 'shopManager' || !shop || shop.shopManager.toString() !== req.user.userId) {
-        return res.status(403).json({ message: 'Unauthorized: Access is denied.' });
-      }
-      next();
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Server error' });
+  try {
+    const { shopId } = req.params; // קבלת ה-shopId מהפרמטרים של ה-URL
+    const shop = await Shop.findById(shopId);
+
+    if (!shop) {
+      return res.status(404).json({ message: 'Shop not found' });
     }
-  };
-    
+
+    const { userId } = req.user; // קבלת ה-userId מה-req.user (המשתמש המחובר)
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // בדיקה אם המשתמש המחובר הוא מנהל האתר
+    if (user.type === 'siteAdmin') {
+      req.shop = shop; // שמירת החנות בפרמטר req לשימוש בראוט הבא
+      return next();
+    }
+
+    // בדיקה אם המשתמש המחובר הוא shopManager של החנות
+    if (shop.shopManager.toString() === userId) {
+      req.shop = shop; // שמירת החנות בפרמטר req לשימוש בראוט הבא
+      return next();
+    }
+
+    // אם המשתמש לא מנהל האתר ולא מנהל החנות, נחזיר תשובת לא מורשה
+    return res.status(403).json({ message: 'Unauthorized access to this shop' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Server error' });
+  }
+};
+  
 
 module.exports = {
   verifyToken,
